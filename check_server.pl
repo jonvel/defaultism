89d82cbcf835c623b772677343f0a1fcc4a4f521
Jon Velapoldi <jonvel@gmail.com>
2023-09-11 00:28:17 -0400

    adding .gitignore, _slightly_ cleaning up check_server.pl

--- check_server.pl
+++ check_server.pl
@@ -7,7 +7,7 @@ my $container = shift // 'valheim.service';
 my $currentcount = 0;
 my $i = 0;
 my $line = '';
-my @linearr = [];
+my @lineArr = [];
 my $usercount = 0;
 my $logdate;
 my $logtime;
@@ -18,41 +18,54 @@ my %userlog;
 open DOCKERLOG, "docker logs $container |";
 while ($line=<DOCKERLOG>) {
   chomp $line;
+  # if the line contains "Got character ZDOID from" then this is a new session
+  # that has started up.  Includes date, userid and sessionid in the text
+  # string (positions 5/6 (date), 11 (userid) 13 (sessionid) starting at 0)
+  # Note that the sessionid can look like [-]\d+:\d+ (optiona -, then a number,
+  # then a : and then a number.  It's a weird number that seems random in its
+  # format.
   if ($line =~ m/Got character ZDOID from/) {
-    @linearr = split(/\s+/, $line);
-    ($logdate, $logtime, $userid, $sessionid)=@linearr[5, 6, 11, 13];
+    @lineArr = split(/\s+/, $line);
+    ($logdate, $logtime, $userid, $sessionid)=@lineArr[5, 6, 11, 13];
+    # logtime ends in a `:`.  Trim that off.
     chop($logtime);
     $userlog{$sessionid}{'userid'}=$userid;
-    $userlog{$sessionid}{'login'}=$logdate."-".$logtime;
-    $userlog{$sessionid}{'logoff'}="NOT YET";
+    $userlog{$sessionid}{'logon'}=$logdate.".".$logtime;
+    $userlog{$sessionid}{'logoff'}="STILL LOGGED IN";
     $usercount += 1;
-    $userlog{$sessionid}{'logincount'} = $usercount;
+    $userlog{$sessionid}{'logoncount'} = $usercount;
+    $userlog{$sessionid}{'logoffcount'} = $usercount;
     #print "logdate $logdate logtime $logtime userid $userid sessionid $sessionid count $usercount\n"
   }
+  # if the line contains "Destroying abandoned non persistent zdo", then this
+  # is a termination of an existing session, referenced by the sessionid.
   elsif ($line =~ m/Destroying abandoned non persistent zdo /) {
-    #print "$line\n";
-    @linearr = split(/\s+/, $line);
-    ($logdate, $logtime, $sessionid)=@linearr[5, 6, 12];
-    chop($logtime);
+    @lineArr = split(/\s+/, $line);
+    ($logdate, $logtime, $sessionid)=@lineArr[5, 6, 12];
+    # Note that there can be a whole bunch of these lines for a single session
+    # termination.  The one we're interested in is one that matches an already
+    # existing logon sessionid.
     if (exists $userlog{$sessionid}) {
-      $userlog{$sessionid}{'logoff'}=$logdate."-".$logtime;
+      # logtime ends in a `:`.  Trim that off.
+      chop($logtime);
+      $userlog{$sessionid}{'logoff'} = $logdate.".".$logtime;
       $usercount -= 1;
-      $userlog{$sessionid}{'logoffcount'}=$usercount;
+      $userlog{$sessionid}{'logoffcount'} = $usercount;
       #print "logdate $logdate logtime $logtime sessionid $sessionid userid $userid count $usercount\n"
     }
   }
 }
 close(DOCKERLOG);
 
-# bad hack to print data by date of "event".
+# bad hack to sort then print data by date of "event".
 # first read entire hash flattened somewhat into an array.  This looks like
-# [ datetime_of_event, username, 'login/out at:', count_current_users ]
+# [ datetime_of_event, username, 'logon/out at:', count_current_users ]
 # Each sessionid will have a "logged in" event (which includes the date and the 
 # userid logged in, and will have a "logged out" event which only has the 
-# sessionid.
+# sessionid.  The userid has to be inferred.
 my @printarr;
 foreach $sessionid (keys %userlog) {
-  @printarr[$i] = [("$userlog{$sessionid}{'login'}", "$userlog{$sessionid}{'userid'}", 'login', $userlog{$sessionid}{'logincount'})]; 
+  @printarr[$i] = [("$userlog{$sessionid}{'logon'}", "$userlog{$sessionid}{'userid'}", 'logon', $userlog{$sessionid}{'logoncount'})]; 
   $i++;
   @printarr[$i] = [("$userlog{$sessionid}{'logoff'}", "$userlog{$sessionid}{'userid'}", 'logoff', $userlog{$sessionid}{'logoffcount'})]; 
   $i++;
@@ -64,7 +77,7 @@ foreach $sessionid (keys %userlog) {
 # that's what we sort by - namely the dereferenced value of the first 
 # element of the array.
 my @by_date = sort {$a->[0] cmp $b->[0]} @printarr;
-printf("%-10s %-7s %-20s %-4s\n", "UserId", "Status", "Date-Time", "usercount");
+printf("%-10s %-7s %-20s %-4s\n", "UserId", "Status", "mm/dd/YYYY.HH:MM:SS", "usercount");
 for my $row (@by_date) {
   printf("%-10s %-7s %-20s %-4d\n", $row->[1], $row->[2], $row->[0], $row->[3]);
 }
